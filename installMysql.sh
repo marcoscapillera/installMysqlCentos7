e44f8a9c517beb2950b2055a79f78ac25e7f0764
Marcos Capillera <62310940+marcoscapillera@users.noreply.github.com>
2020-10-10 21:50:01 -0300

    Update installMysql.sh

--- installMysql.sh
+++ installMysql.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 # Description: Set up MySQL Community Release 5.7
 
-echo "se instalara mysql 5.7, espere..."
-sleep 3
-
 # Get the repo RPM and install it.
 yum localinstall https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 
 # Install the server and start it
-yum  install mysql-community-server 
+yum -y install mysql-community-server 
 
 
 # Get the temporary password
-grep password /var/log/mysqld.log | awk '{print $NF}'
-read -p "Presione enter para continuar"
+temp_password=$(grep password /var/log/mysqld.log | awk '{print $NF}')
 
 # Set up a batch file with the SQL commands
-echo "Debes log con la pass temporal con el siguiente comando mysql -u root -pastemporal"
-read -p "Presione enter para continuar"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Welcome-123'; flush privileges;" > reset_pass.sql
 
 # Log in to the server with the temporary password, and pass the SQL file to it.
-echo "Ingrese new password"
-read pass
-
-alter user 'root'@'localhost' identified by '$pass';
+mysql -u root --password="$temp_password" --connect-expired-password < reset_pass.sql
 
 service mysqld start
 service mysqld status
