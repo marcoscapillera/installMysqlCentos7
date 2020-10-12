25f6ce545f333b104afd502371cbcf6eceef3175
Marcos Capillera <62310940+marcoscapillera@users.noreply.github.com>
2020-10-11 22:08:21 -0300

    Update installMysql.sh

--- installMysql.sh
+++ installMysql.sh
@@ -3,7 +3,9 @@ yum localinstall https://dev.mysql.com/get/mysql57-community-release-el7-9.noarc
 read -p "Presione enter para continuar"
 #yum install mysql-community-server
 #read -p "Presione enter para continuar"
+#service mysqld start
 #grep 'A temporary password' /var/log/mysqld.log |tail -1
+
 #read -p "Ingrese a mysql con mysql -u root -p passwordtemp"
 #mysql -u root -ppassword
 #alter user 'root'@'localhost' identified by 'password';
